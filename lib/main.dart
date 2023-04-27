f1067a4d924f5804b56e18534f3e854cd3777405
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-04-27 09:46:17 -0400

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp() );
+  runApp( const MyApp() );
 }
 
 class MyApp extends StatelessWidget {
+  
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hello World')),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text('Hello World')),
+      ),
     );
   }
 
